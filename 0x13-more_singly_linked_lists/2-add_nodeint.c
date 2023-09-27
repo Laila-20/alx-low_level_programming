6e267d6d561db849c783b798d2b8e3c22774910e
Laila-20 <lsarhane12@gmail.com>
2023-09-27 09:59:04 -0700

    4,5 and 6 tasks

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,15 +1,17 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a node to the top
- * @head: pointer to the first node
- * @n: value for new node
+ * add_nodeint - adds a new node at the beginning of a linked list
+ * @head: pointer to the first node in the list
+ * @n: data to insert in that new node
  *
- * Return: pointer to new node
+ * Return: pointer to the new node, or NULL if it fails
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
 
--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
 /**
- * free_listint - frees a list of ints
+ * free_listint - frees a linked list
  * @head: listint_t list to be freed
  */
 void free_listint(listint_t *head)
 {
-	listint_t *node;
+	listint_t *temp;
 
 	while (head)
 	{
-		head = head->next;
-		free(node);
-		node = head;
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
 }
